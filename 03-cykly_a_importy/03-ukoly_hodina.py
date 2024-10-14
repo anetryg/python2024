c9a0215c0e6ea3ec03476a41d17b1aacbb0cb4c9
RYGLOVA Aneta <anetryglova@seznam.cz>
2024-10-14 14:07:53 +0200

    cv3

---
+++ 03-cykly_a_importy/03-ukoly_hodina.py
@@ -0,0 +1,66 @@
+#Úkol 1
+""" Vytvořte seznam jmen. Za každé jméno chceme přidat pomocí cyklu konec e-mailové adresy (například máme v seznamu jméno "Aneta" a chceme za něj přidat "@gmail.com"). Vypište maily na obrazovku. """
+
+
+#Úkol 2
+""" Vytvořte seznam produktů a k jejich názvům pomocí cyklu přidejte čísla (např. "Jablko 1", "Banán 2" atd.) pomocí cyklu. Vypište výsledek na obrazovku. """
+
+
+
+#Úkol 3
+""" Vytvořte seznam školních známek. Následně pomocí cyklu spočítejte počet jedniček. """
+
+
+
+#Úkol 4
+""" Vytvořte program, který spočítá součet všech lichých čísel v zadaném seznamu čísel."""
+        
+
+
+#Úkol 5
+""" Vytvořte program, který uživateli pomocí cyklu while umožní hádat číslo, které si počítač "myslí" (uložte na začátku libovolné číslo do proměnné). Uživatel hádá, dokud se netrefí. Pak se program ukončí. """
+
+
+#Úkol 6
+""" Rozšiřte předchozí program tak, aby uživatel dostával zpětnou vazbu, zda je číslo větší nebo menší"""
+
+
+
+
+#Úkol 7
+""" Vytvořte program, který pomocí cyklu spočítá součet všech čísel v seznamu. """
+
+
+
+#Úkol 8
+""" Máte seznam hesel viz níže. Pomocí cyklu vypište všechny hesla na obrazovku. Upravte váš program tak, aby vypisoval jen bezpečná hesla, tedy taková, jež jsou delší než 8 znaků."""
+
+hesla = [
+    "xyz101",
+    "punťa",
+    "razor-blade",
+    "1234",
+    "12011986",
+    "martin111",
+    "trhnisi",
+    "hokuspokus",
+    "jeník15",
+    "kristus-te-spasi",
+    "beruška",
+    "strčprstskrzkrk",
+]
+
+
+#Úkol 9
+""" Napište cyklus, který projde zadaný seznam desetinných čísel a spočítá jejich průměr. Seznam čísel si vytvořte na začátku programu. """
+
+
+#Úkol 10
+""" Napište cyklus, který projde zadaný seznam celých čísel a najde v něm největší hodnotu. """
+
+
+
+ 
+ 
+
+
